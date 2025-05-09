9c99ee1e8fc3d856b738039dc4b69bc30e36585c
Emin Basbayan <emin.basbayan@icloud.com>
2025-05-09 16:35:42 +0300

    javascript konsol bildirim ve kullanıcı etkileşimi

---
+++ 03_javascript_dersleri/01_javascript_temelleri/08_konsol_bildirim_etkilesim/script.js
@@ -0,0 +1,24 @@
+5 + 9;
+console.log(5 + 6);
+console.log("Toplam:", 20 + 10, "TL");
+
+const hayvanlar = ["Kedi", "Köpek", "Kuş"];
+
+console.table(hayvanlar);
+
+let ogrenciler = [
+  { ad: "Ayşe", yas: 17 },
+  { ad: "Mehmet", yas: 18 },
+  { ad: "Zeynep", yas: 16 },
+];
+
+console.table(ogrenciler);
+
+console.warn("Dikkat! Şarj azalıyor.");
+console.error("Sunucuya bağlanılamadı!");
+
+alert("Sayfaya hoş geldiniz!");
+
+const isim = prompt("Adın ne?");
+
+console.log("Hoş geldin", isim);
